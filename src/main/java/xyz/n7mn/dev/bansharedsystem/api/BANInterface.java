ea2736d0d03c93d6903d45854cfdbd01f713c3e1
kanasaki15 <kassi_0123@kassi-hp.info>
2020-08-13 01:46:37 +0900

    Initial commit

---
+++ src/main/java/xyz/n7mn/dev/bansharedsystem/api/BANInterface.java
@@ -0,0 +1,8 @@
+package xyz.n7mn.dev.bansharedsystem.api;
+
+public interface BANInterface {
+
+    void init();
+    void run();
+    void close();
+}
---
+++ src/main/java/xyz/n7mn/dev/bansharedsystem/api/LocalBAN.java
@@ -0,0 +1,18 @@
+package xyz.n7mn.dev.bansharedsystem.api;
+
+public class LocalBAN implements BANInterface {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
