9176fc078050c4324cd6f4c6e012c29f8e8f5d3b
kanasaki15 <kassi_0123@kassi-hp.info>
2020-08-14 02:12:15 +0900

    ShareBAN BugFix

--- src/main/java/xyz/n7mn/dev/bansharedsystem/api/ShareBAN.java
+++ src/main/java/xyz/n7mn/dev/bansharedsystem/api/ShareBAN.java
@@ -1,11 +1,14 @@
 package xyz.n7mn.dev.bansharedsystem.api;
 
 import com.google.gson.Gson;
+
+import java.io.*;
 import java.util.Base64;
+
+import org.bukkit.BanList;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
@@ -43,22 +46,24 @@ public class ShareBAN implements BANInterface {
 
         try {
             if (isBan){
+                boolean run = new LocalBAN().run(fromPlayer, targetPlayer, reason, expirationDate, isBan);
 
-                if (!new LocalBAN().run(fromPlayer, targetPlayer, reason, expirationDate, isBan)){
+                if (!run){
                     return false;
                 }
 
                 String format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(expirationDate);
-                String str = Base64.getEncoder().encode(new Gson().toJson(new BanShareJson(authData.getServerUUID(), targetPlayer.getUniqueId(), reason, format, fromPlayer.getUniqueId())).getBytes()).toString();
 
-                ByteArrayOutputStream obj = new ByteArrayOutputStream();
-                GZIPOutputStream gzip = new GZIPOutputStream(obj);
-                gzip.write(str.getBytes(StandardCharsets.UTF_8));
-                gzip.close();
-                str = obj.toString("UTF-8");
-                obj.close();
+                BanShareJson banShareJson = new BanShareJson(authData.getServerUUID(), targetPlayer.getUniqueId(), reason, format, fromPlayer.getUniqueId());
 
-                return new Gson().fromJson(new Http().get(APIURL.BaseURL + APIURL.Version + APIURL.BanShareAdd + URLEncoder.encode(str,"UTF-8")), boolean.class);
+                byte[] encode = Base64.getEncoder().encode(new Gson().toJson(banShareJson).getBytes(StandardCharsets.UTF_8));
+
+                boolean json = json = new Gson().fromJson(new Http().get(APIURL.BaseURL + APIURL.Version + APIURL.BanShareAdd + URLEncoder.encode(new String(encode), "UTF-8")), boolean.class);
+
+                if (!json){
+                    Bukkit.getServer().getBanList(BanList.Type.NAME).pardon(targetPlayer.getName());
+                }
+                return json;
             } else {
                 return new Gson().fromJson(new Http().get(APIURL.BaseURL + APIURL.Version + APIURL.BanShareRemove + URLEncoder.encode(fromPlayer.getUniqueId().toString(),"UTF-8")+"&s_uuid="+URLEncoder.encode(authData.getServerUUID().toString(), "UTF-8")), boolean.class);
             }
